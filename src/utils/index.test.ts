10d8afdca7faad578310492ed7eda736922b3892
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-02-21 16:06:49 +0330

    feat: test utility functions

---
+++ src/utils/index.test.ts
@@ -0,0 +1,52 @@
+import { createRangeArray, getAlphabetCharAtIndex, isValidNumber } from '.';
+
+describe('createRangeArray', () => {
+  test('should return an array from 0 to 10', () => {
+    const range = createRangeArray(10);
+    for (let i = 0; i < 10; i++) {
+      expect(range[i]).toEqual(i);
+    }
+  });
+  test('should return an empty array', () => {
+    const range = createRangeArray(0);
+    expect(range.length).toEqual(0);
+  });
+});
+
+describe('getAlphabetCharAtIndex', () => {
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  test('should return the alphabet in order', () => {
+    for (let i = 0; i < 26; i++) {
+      expect(getAlphabetCharAtIndex(i)).toEqual(ALPHABET.charAt(i));
+    }
+  });
+
+  test('should return the alphabet from the beginning if the the index is over 26', () => {
+    for (let i = 0; i < 26; i++) {
+      expect(getAlphabetCharAtIndex(26 + i)).toEqual(ALPHABET.charAt(i));
+    }
+  });
+});
+
+describe('isValidNumber', () => {
+  test('should return true for strings with only digits', () => {
+    expect(isValidNumber('123')).toBeTruthy();
+  });
+
+  test('should return false for strings with no digits', () => {
+    expect(isValidNumber('one')).toBeFalsy();
+    expect(isValidNumber('two')).toBeFalsy();
+    expect(isValidNumber('')).toBeFalsy();
+  });
+
+  test('should return true for strings with floating point numbers', () => {
+    expect(isValidNumber('123.45')).toBeTruthy();
+    expect(isValidNumber('123.')).toBeTruthy();
+    expect(isValidNumber('.123')).toBeTruthy();
+  });
+
+  test('should return true for strings with negative and positive symbols', () => {
+    expect(isValidNumber('+123.45')).toBeTruthy();
+    expect(isValidNumber('-123')).toBeTruthy();
+  });
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,8 @@ const CHAR_CODE_A = 65;
 export const getAlphabetCharAtIndex = (index: number): string => {
   return String.fromCharCode(CHAR_CODE_A + (index % 26));
 };
+
+const NUMBER_REGEX = new RegExp(/^(-|\+)?((\d+)\.?(\d+)?|(\d+)?\.?(\d+))$/);
+export const isValidNumber = (text: string): boolean => {
+  return NUMBER_REGEX.test(text);
+};
